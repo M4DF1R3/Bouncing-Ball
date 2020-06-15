e60bdf078d6af555343a40842575369cf19ce4fe
M4DF1R3 <36823677+M4DF1R3@users.noreply.github.com>
2020-06-15 11:35:43 -0600

    Add files via upload

---
+++ Pythagoreon Calc.py
@@ -0,0 +1,6 @@
+import math
+def calc():
+    s1 = float(input("Enter the first side of the triangle"))
+    s2 = float(input("Enter the second side of the triangle"))
+    print(math.sqrt(s1**2 + s2**2))
+calc()
